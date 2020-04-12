e388a287adf04147f6e8e9f370f14056cb0a3bba
Rajan Gyawali <rajangyawali93@gmail.com>
2020-04-12 09:09:48 +0545

    Image Update

--- NepalTech/settings.py
+++ NepalTech/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'NepalTech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'NepalTech',
+        'USER': 'postgres',
+        'PASSWORD': 'raju',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
